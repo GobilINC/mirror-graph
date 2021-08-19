59b1c79cb220b4bb844a9a457cd71c259efe8681
Alex Kim <alex@terra.money>
2021-08-19 16:23:15 +0900

    fix(collector): improve cdp ratio calculation performence

--- src/collector/cdp.ts
+++ src/collector/cdp.ts
@@ -1,7 +1,9 @@
 import { getManager, EntityManager } from 'typeorm'
 import * as bluebird from 'bluebird'
-import { assetService, oracleService, priceService } from 'services'
+import { assetService, oracleService, collateralService } from 'services'
 import { CdpEntity } from 'orm'
+
+let lastUpdatedAll = 0
 
 async function removeClosedCdps(manager: EntityManager): Promise<void> {
   await manager
@@ -12,7 +14,7 @@ async function removeClosedCdps(manager: EntityManager): Promise<void> {
     .execute()
 }
 
-async function updateCdpRatio(manager: EntityManager): Promise<void> {
+async function updateCdpRatio(manager: EntityManager, maxDiffRatio?: number): Promise<void> {
   const assets = await assetService().getListedAssets()
   const collateralAssets = await assetService().getCollateralAssets()
 
@@ -26,57 +28,74 @@ async function updateCdpRatio(manager: EntityManager): Promise<void> {
         return
       }
 
-      await manager
+      let qb = manager
         .createQueryBuilder()
         .update(CdpEntity)
         .set({ mintValue: () => `mint_amount * ${oraclePrice}` })
         .where('token = :token', { token })
-        .andWhere('collateral_amount > 0 AND mint_amount > 0')
-        .execute()
+        .andWhere('mint_amount > 0')
+
+      if (maxDiffRatio) {
+        qb = qb.andWhere('collateral_ratio - min_collateral_ratio < :maxDiffRatio', { maxDiffRatio })
+      }
+
+      await qb.execute()
     }
   )
 
   // update collateral value
   await bluebird.mapSeries(
-    [...assets, ...collateralAssets],
+    [...assets, ...collateralAssets].filter((asset) => asset.symbol !== 'uusd'),    
     async (asset) => {
-      const { token, symbol } = asset
-      let tokenPrice
-
-      if (symbol === 'MIR') {
-        tokenPrice = await priceService().getPrice(token)
-      } else if (symbol === 'uusd') {
-        tokenPrice = '1'
-      } else {
-        tokenPrice = await oracleService().getPrice(token)
-      }
-
-      if (!tokenPrice) {
+      const { token } = asset
+      const collateralPrice = await collateralService().getPrice(token)
+      if (!collateralPrice) {
         return
       }
 
-      await manager
+      let qb = manager
         .createQueryBuilder()
         .update(CdpEntity)
-        .set({ collateralValue: () => `collateral_amount * ${tokenPrice}` })
+        .set({ collateralValue: () => `collateral_amount * ${collateralPrice}` })
         .where('collateral_token = :token', { token })
-        .andWhere('collateral_amount > 0 AND mint_amount > 0')
-        .execute()
+        .andWhere('collateral_amount > 0')
+
+      if (maxDiffRatio) {
+        qb = qb.andWhere('collateral_ratio - min_collateral_ratio < :maxDiffRatio', { maxDiffRatio })
+      }
+
+      await qb.execute()
     }
   )
 
   // update collateral ratio
-  await manager
+  let qb = manager
     .createQueryBuilder()
     .update(CdpEntity)
     .set({ collateralRatio: () => `collateral_value / mint_value` })
     .where('collateral_value > 0 AND mint_value > 0')
-    .execute()
+
+  if (maxDiffRatio) {
+    qb = qb.andWhere('collateral_ratio - min_collateral_ratio < :maxDiffRatio', { maxDiffRatio })
+  }
+
+  await qb.execute()
 }
 
 export async function updateCdps(): Promise<void> {
+  const now = Date.now()
+
   await getManager().transaction(async (manager: EntityManager) => {
     await removeClosedCdps(manager)
-    await updateCdpRatio(manager)
+
+    if (now - lastUpdatedAll > 60000) {
+      // update all cdp every 1min
+      await updateCdpRatio(manager)
+
+      lastUpdatedAll = now
+    } else {
+      // close to liquidation(< 15%) cdps update every block
+      await updateCdpRatio(manager, 0.15)
+    }
   })
 }
